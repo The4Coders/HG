b23fda7f4c2c8a0aa91694baf8f2fece90279f5f
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-12 15:29:53 +0100

    Test Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const page = () => {
   return (
     <div className="text-4xl text-green-500 text-center my-4">
-      Landing Page Here
+      Landing Page Was edited
+      <h2>Testing Commit</h2>
     </div>
   );
 };
