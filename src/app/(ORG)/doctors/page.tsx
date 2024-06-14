14f2c3cb08a6edd8014fadf47fd53ed1d1aabc4b
Obua Stella <stellaobua123@gmail.com>
2024-06-14 01:40:55 +0100

    Added doctors page to org dashboard

---
+++ src/app/(ORG)/doctors/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import OrgLayout from "@/components/orgLayout";
+
+export default function Page() {
+  return (
+    <main>
+      <OrgLayout>
+        <main className=" bg-white w-full">
+          <h2>Doctors</h2>
+        </main>
+      </OrgLayout>
+    </main>
+  );
+}
