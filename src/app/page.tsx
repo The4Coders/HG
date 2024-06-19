fc90a318a029b93bee35b3f8e42e8589a3d9f589
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-19 09:06:21 +0100

    Removed HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "./LandingPage/Navbar/navbar";
-import HeroSection from "./LandingPage/Sections/heroSection";
+// import HeroSection from "./LandingPage/Sections/heroSection";
 // import SecondSection from "./LandingPage/Sections/secondSection";
 // import CarexaSection from "./LandingPage/Sections/carexaSection";
 // import FourthSection from "./LandingPage/Sections/fourthSection";
