349114bce6a47e0c35733f2d5f8be60dcaa1243a
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-19 07:31:01 +0100

    Adding Components 1 by 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import Navbar from "./LandingPage/Navbar/navbar";
-import HeroSection from "./LandingPage/Sections/heroSection";
-import SecondSection from "./LandingPage/Sections/secondSection";
-import CarexaSection from "./LandingPage/Sections/carexaSection";
-import FourthSection from "./LandingPage/Sections/fourthSection";
-import AnimationSection from "./LandingPage/Sections/animationSection";
-import Footer from "./LandingPage/Sections/footer";
+// import HeroSection from "./LandingPage/Sections/heroSection";
+// import SecondSection from "./LandingPage/Sections/secondSection";
+// import CarexaSection from "./LandingPage/Sections/carexaSection";
+// import FourthSection from "./LandingPage/Sections/fourthSection";
+// import AnimationSection from "./LandingPage/Sections/animationSection";
+// import Footer from "./LandingPage/Sections/footer";
 
 export default function page() {
   return (
     <main className="w-full overflow-hidden">
       <Navbar />
-      <HeroSection />
+      {/* <HeroSection />
       <SecondSection />
       <CarexaSection />
       <FourthSection />
       <AnimationSection />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
