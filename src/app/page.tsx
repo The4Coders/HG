b8d2c4e627b83b3646679a49f4f5060b8aa82005
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-19 11:30:09 +0100

    Removing Bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
 import Navbar from "./LandingPage/Navbar/navbar";
-import HeroSection from "./LandingPage/Sections/heroSection";
-import CarexaSection from "./LandingPage/Sections/carexaSection";
-import FourthSection from "./LandingPage/Sections/fourthSection";
-import AnimationSection from "./LandingPage/Sections/animationSection";
-import Footer from "./LandingPage/Sections/footer";
 
 export default function page() {
   return (
     <main className="w-full overflow-hidden">
       <Navbar />
-      <HeroSection />
-      <CarexaSection />
-      <FourthSection />
-      <AnimationSection />
-      <Footer />
     </main>
   );
 }
