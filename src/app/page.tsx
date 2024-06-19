265ada405bb10ba81439ac659322327b83478b1d
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-19 08:00:24 +0100

    Added HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "./LandingPage/Navbar/navbar";
-// import HeroSection from "./LandingPage/Sections/heroSection";
+import HeroSection from "./LandingPage/Sections/heroSection";
 // import SecondSection from "./LandingPage/Sections/secondSection";
 // import CarexaSection from "./LandingPage/Sections/carexaSection";
 // import FourthSection from "./LandingPage/Sections/fourthSection";
@@ -10,7 +10,8 @@ export default function page() {
   return (
     <main className="w-full overflow-hidden">
       <Navbar />
-      {/* <HeroSection />
+      <HeroSection />
+      {/* 
       <SecondSection />
       <CarexaSection />
       <FourthSection />
