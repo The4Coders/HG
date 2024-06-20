fd99dd0d981de75f43f1e3be912486ca78c50393
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-20 21:28:36 +0100

    Added HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./LandingPage/Navbar/navbar";
+import HeroSection from "./LandingPage/Sections/heroSection";
 
 export default function page() {
   return (
     <main className="w-full overflow-hidden">
       <Navbar />
+      <HeroSection />
     </main>
   );
 }
