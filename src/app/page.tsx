d0d4f99af24825a15bcc2bf21175f162b6e4273e
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-20 22:05:04 +0100

    Disabling SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Navbar from "./LandingPage/Navbar/navbar";
-import HeroSection from "./LandingPage/Sections/heroSection";
+import dynamic from "next/dynamic";
+
+// Dynamically import the HeroSection with SSR disabled
+const DynamicHeroSection = dynamic(
+  () => import("./LandingPage/Sections/heroSection"),
+  {
+    ssr: false,
+  }
+);
 
 export default function page() {
   return (
     <main className="w-full overflow-hidden">
       <Navbar />
-      <HeroSection />
+      <DynamicHeroSection />
     </main>
   );
 }
